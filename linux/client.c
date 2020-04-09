59a91bbfee8b9566065ba3ed9f67726417368c4d
LachlanPond <lachipond@gmail.com>
2020-04-10 03:38:29 +1000

    updated IP

--- linux/client.c
+++ linux/client.c
@@ -15,7 +15,7 @@ int main() {
     struct sockaddr_in server_address;
     server_address.sin_family = AF_INET;
     server_address.sin_port = htons(9002);
-    server_address.sin_addr.s_addr = inet_addr("13.211.3.157");//INADDR_ANY;    // Basically the same as saying the address is localhost
+    server_address.sin_addr.s_addr = inet_addr("52.62.229.41");//INADDR_ANY;    // Basically the same as saying the address is localhost
 
     // Connect to the server
     int connection_status = connect(network_socket, (struct sockaddr *) &server_address, sizeof(server_address));
